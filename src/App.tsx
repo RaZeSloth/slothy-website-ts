43d3f228897ff9429950653e2fd68bba4b33f6c7
RaZe <75721601+RaZegame@users.noreply.github.com>
2021-07-10 01:41:05 +0300

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
 import React from 'react';
 import './App.css';
-
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import profile from './profile';
 function App() {
   return (
+
     <div className="App">
+
+
+
+
       <header className="App-header">
+        <Router>
+          <Route path="/profile" component={profile} />
+
+        </Router>
+        <a href="/profile" className="log" >
+          sussy
+        </a>
         <img src="https://cdn.discordapp.com/avatars/826056261610373130/7898450388fd94cf5a442cf6e0d1ee13.webp?size=4096" className="App-logo" alt="sloth_here" />
         <a
           className="App-link"
---
+++ src/profile.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+const { Database } = require("quickmongo");
+const db = new Database(" ")
+const profile = () => { 
+   return (
+      <div>
+         
+         <h1>Profile</h1>
+         <p>Still wip</p>
+      </div>
+   );
+}
+
+export default profile;
+
